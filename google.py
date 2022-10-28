962c9e1a907d2352137c02df422b51ab16f163f6
Manuel Eugenio Losada Clua <ManuelEugenio.LosadaClua@endava.com>
2022-10-28 16:59:52 -0300

    Agregar buscar por CSS y por XPATH

--- google.py
+++ google.py
@@ -1,4 +1,3 @@
-from lib2to3.pgen2 import driver
 from selenium import webdriver 
 from selenium.webdriver.common.by import By 
 import time 
@@ -7,5 +6,7 @@ driver = webdriver.Chrome('chromedriver.exe')
 driver.get('https://www.google.com')
 time.sleep(2)
 
-search_box = driver.find_element(By.NAME,'q')
+#search_box = driver.find_element(By.NAME,'q')
+#search_box = driver.find_element(By.XPATH,'/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input')
+search_box = driver.find_element(By.CSS_SELECTOR,'body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf > div.RNNXgb > div > div.a4bIc > input')
 search_box.send_keys('Python selenium')
